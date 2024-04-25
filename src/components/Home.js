a5479931c13c26a780bfb95dcae3f11646da91fe
brandondando <dandobrandon0@gmail.com>
2024-04-25 06:39:28 +0300

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React from 'react'
+import { username, city } from '../data/user';
 
 function Home() {
+
   return (
     <div id="home">
       <h1>
@@ -9,3 +11,6 @@ function Home() {
     </div>
   );
 }
+
+export default Home
+
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from 'react'
 
 function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
     </nav>
-  );
+  )
 }
+
+export default NavBar
+
+
+
+
